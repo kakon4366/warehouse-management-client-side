416d151fc168bde575554f744329f1e4d203e689
kakon4366 <kakonbarman4366@gmail.com>
2022-04-29 18:16:10 +0600

    Firebase install and env file setup

---
+++ src/Pages/SingUp/SignUp.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SignUp = () => {
+	return (
+		<section>
+			<div className="container mx-auto">
+				<div>
+					<h3>Sing Up</h3>
+					<form action="">
+						<label htmlFor="name">Name</label>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default SignUp;
