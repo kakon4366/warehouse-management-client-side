b32dde42b1c8f289a7f3b84ea7819165ed527c6b
kakon4366 <kakonbarman4366@gmail.com>
2022-05-01 22:29:17 +0600

    Pagination add success

--- src/Pages/Inventories/Inventories/Inventories.js
+++ src/Pages/Inventories/Inventories/Inventories.js
@@ -1,9 +1,29 @@
-import React from "react";
-import useProducts from "../../../Hooks/useProducts";
+import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 
 const Inventories = () => {
-	const [products] = useProducts();
+	const [products, setProducts] = useState([]);
+	const [pageCount, setPageCount] = useState(0);
+	const [page, setPage] = useState(0);
+	const [limit, setLimit] = useState(5);
+
+	//get all products
+	useEffect(() => {
+		fetch(`http://localhost:5000/productsList?page=${page}&limit=${limit}`)
+			.then((res) => res.json())
+			.then((data) => setProducts(data));
+	}, [page, limit]);
+
+	//get products count
+	useEffect(() => {
+		fetch("http://localhost:5000/productsCount")
+			.then((res) => res.json())
+			.then((result) => {
+				const count = result.count;
+				const page = Math.ceil(count / 3);
+				setPageCount(page);
+			});
+	}, []);
 
 	return (
 		<section className="pt-20 py-10">
@@ -13,6 +33,29 @@ const Inventories = () => {
 					{products.map((product) => (
 						<Product product={product} key={product._id}></Product>
 					))}
+				</div>
+				<div className="mt-8">
+					{[...Array(pageCount).keys()].map((num) => (
+						<button
+							className={`border p-2 border-orange-500 mr-4 ${
+								page === num ? "bg-orange-500" : ""
+							}`}
+							onClick={() => setPage(num)}
+							key={num}
+						>
+							{num + 1}
+						</button>
+					))}
+					<select
+						className="border-orange-500 border p-2"
+						defaultValue={5}
+						onChange={(e) => setLimit(e.target.value)}
+					>
+						<option value="5">5</option>
+						<option value="10">10</option>
+						<option value="15">15</option>
+						<option value="20">20</option>
+					</select>
 				</div>
 			</div>
 		</section>
